42bd26bf5f4a71dd26857bfc60bbb0270f8ac98e
saksham030803 <sakshambhatnagar308@gmail.com>
2024-10-14 18:45:20 +0530

    saksham

--- packages/main.go
+++ packages/main.go
@@ -3,4 +3,8 @@ package main
 // All files in the same directory must belong to the same package.
 
 // When Go compiles a package, it expects all files in that directory to be part of the same package, which means they must share the same package declaration at the top of each file.
-func main() {}
+
+func main() {
+//     a1:=users.User{"Salsjam","sakn"}
+//     fmt.Println(a1)
+ }
---
+++ pointers/go.mod
@@ -0,0 +1,3 @@
+module poi
+
+go 1.21.5
